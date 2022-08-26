5d84acd20e91b59626f75596983b288a657da0ed
Jackson Santos Nascimento <jacksonnascimento84@hotmail.com>
2022-08-25 23:13:39 -0300

    Update Caixa.java

--- Sistema_de_faturamento/src/main/java/com/mycompany/sistema_de_faturamento/Caixa.java
+++ Sistema_de_faturamento/src/main/java/com/mycompany/sistema_de_faturamento/Caixa.java
@@ -9,5 +9,51 @@ package com.mycompany.sistema_de_faturamento;
  * @author Jackson
  */
 public class Caixa {
-    
+   private int id;
+   private double valorDoCaixa;
+   private double valorTotalDesspesas;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the valorDoCaixa
+     */
+    public double getValorDoCaixa() {
+        return valorDoCaixa;
+    }
+
+    /**
+     * @param valorDoCaixa the valorDoCaixa to set
+     */
+    public void setValorDoCaixa(double valorDoCaixa) {
+        this.valorDoCaixa = valorDoCaixa;
+    }
+
+    /**
+     * @return the valorTotalDesspesas
+     */
+    public double getValorTotalDesspesas() {
+        return valorTotalDesspesas;
+    }
+
+    /**
+     * @param valorTotalDesspesas the valorTotalDesspesas to set
+     */
+    public void setValorTotalDesspesas(double valorTotalDesspesas) {
+        this.valorTotalDesspesas = valorTotalDesspesas;
+    }
+   
+   
 }
