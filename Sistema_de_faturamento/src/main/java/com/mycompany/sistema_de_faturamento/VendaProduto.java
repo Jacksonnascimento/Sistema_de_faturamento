f2e58be4e43bb9095ffae624e24f4b209ca79614
Jacksonnascimento <jacksonnascimento84@hotmail.com>
2022-10-07 18:48:48 -0300

    Update VendaProduto.java

--- Sistema_de_faturamento/src/main/java/com/mycompany/sistema_de_faturamento/VendaProduto.java
+++ Sistema_de_faturamento/src/main/java/com/mycompany/sistema_de_faturamento/VendaProduto.java
@@ -68,10 +68,11 @@ public class VendaProduto {
         
         ArrayList<VendaProduto> vendasProduto = new ArrayList<>();
             
-        String select = String.format("SELECT CC.*, C.DATA_DA_COMPRA FROM COMPRA_CLIENTE CC INNER JOIN COMPRAS C ON CC.COD_DA_COMPRA = C.COD_DA_COMPRA\n" +
-                        "WHERE CC.COD_DA_COMPRA = \n" +
-                        "(SELECT TOP 1 COD_DA_COMPRA FROM COMPRAS WHERE ID_CLIENTE = CC.ID_CLIENTE ORDER BY DATA_DA_COMPRA DESC)\n" +
-                        "AND CC.ID_CLIENTE = %s", idCliente);
+        String select = String.format("""
+                                      SELECT CC.*, C.DATA_DA_COMPRA FROM COMPRA_CLIENTE CC INNER JOIN COMPRAS C ON CC.COD_DA_COMPRA = C.COD_DA_COMPRA
+                                      WHERE CC.COD_DA_COMPRA = 
+                                      (SELECT TOP 1 COD_DA_COMPRA FROM COMPRAS WHERE ID_CLIENTE = CC.ID_CLIENTE ORDER BY DATA_DA_COMPRA DESC)
+                                      AND CC.ID_CLIENTE = %s""", idCliente);
         
         select = banco.select(select, 5);
         
